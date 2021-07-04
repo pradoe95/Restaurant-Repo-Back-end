c5988367feca4bcdbb93ddab67c26d4663745759
dshwang1 <yoonseok115@gmail.com>
2021-07-03 20:48:21 -0400

    Update RestaurantRepository.java
    bug fixed

--- restaurant-spring-boot/src/main/java/com/cognixia/jump/repository/RestaurantRepository.java
+++ restaurant-spring-boot/src/main/java/com/cognixia/jump/repository/RestaurantRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.cognixia.jump.model.Restaurant;
@@ -13,7 +14,7 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
 	
 	
-	@Query("select e from Restaurant e where e.name like '%?1%'")
-	List<Restaurant> findRestaurantsByName(String name);
+	@Query("select e from Restaurant e where e.name like %:name%")
+	List<Restaurant> findRestaurantsByName(@Param("name") String name);
 	
 }
