a6f5c82c0887684e8c4e7f869a7169730d9ba610
dshwang1 <yoonseok115@gmail.com>
2021-07-04 01:19:14 -0400

    Update ReviewController.java
    implemented review controller, but add review method. need some research

--- restaurant-spring-boot/src/main/java/com/cognixia/jump/controller/ReviewController.java
+++ restaurant-spring-boot/src/main/java/com/cognixia/jump/controller/ReviewController.java
@@ -1,11 +1,18 @@
 package com.cognixia.jump.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cognixia.jump.model.Review;
@@ -32,6 +39,58 @@ public class ReviewController {
 	@GetMapping("/reviews")
 	public List<Review> getReviews() {
 		return service.findAll();
+	}
+	
+	//finds a specific review by id
+	@CrossOrigin
+	@GetMapping("/review/{id}")
+	public Review getReview(@PathVariable long id) {
+		Optional<Review> result = service.findById(id);
+		if(result.isPresent())
+			return result.get();
+		
+		return new Review();
+		
+	}
+	
+	
+//	@CrossOrigin
+//	@PostMapping("/review/add/{user_id}/{restaurant_id}")
+//	public void addReview(@)
+	
+	
+	@CrossOrigin
+	@PutMapping("/review/update")
+	public @ResponseBody String updateReview(@RequestBody Review review) {
+		Optional<Review> found = service.findById(review.getReview_id());
+		
+		if(found.isPresent()) {
+			service.save(review);
+			return "review edited";
+		}
+		else {
+			return "Coudn't edit ithe review";
+		}
+	}
+	
+	@CrossOrigin
+	@DeleteMapping("/review/delete/{id}")
+public ResponseEntity<String> deleteStudent(@PathVariable long id) {
+		
+		Optional<Review> found = service.findById(id);
+		
+		if(found.isPresent()) {
+			
+			service.deleteById(id);
+			
+			return ResponseEntity.status(200).body("Deleted review with id = " + id);	
+		}
+		else {
+			return ResponseEntity.status(400)
+					.header("review id", id + "")
+					.body("Review with id = " + id + " not found");
+		}
+			
 	}
 	
 }
